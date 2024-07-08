d50086c0a2811a1f4542f2a348e7fb687623e81a
mahindraD <d.mahindra04010611@gmail.com>
2024-07-08 15:41:30 +0530

    create user updae api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -18,6 +19,8 @@ app.listen(3000, ()=>{
 });
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
