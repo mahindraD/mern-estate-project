ce878880b9f07bf1f1735a0cce3c8fb622316d3c
mahindraD <d.mahindra04010611@gmail.com>
2024-08-05 07:51:57 +0530

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4'>Real Estate</h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus esse dolore similique quae voluptatibus quod dolores nesciunt doloribus sed cupiditate quidem eius reprehenderit magnam enim nulla ducimus, dolorum rerum, quo veniam dicta maxime. Quibusdam nostrum accusamus exercitationem dolorem quas architecto provident asperiores quam? Delectus, similique consequatur illo molestias fugiat mollitia obcaecati provident ipsam minus quod, accusamus eos. Laboriosam velit quisquam earum, similique voluptate voluptas. Autem dolore ab nesciunt doloribus rem inventore, suscipit fugit blanditiis veritatis nihil. Nulla laudantium nemo dignissimos laboriosam eligendi. Aliquid, necessitatibus earum soluta sit rerum ea ad perspiciatis nostrum distinctio ex dolorum perferendis iusto nulla tempora alias.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus esse dolore similique quae voluptatibus quod dolores nesciunt doloribus sed cupiditate quidem eius reprehenderit magnam enim nulla ducimus, dolorum rerum, quo veniam dicta maxime. Quibusdam nostrum accusamus exercitationem dolorem quas architecto provident asperiores quam? Delectus, similique consequatur illo molestias fugiat mollitia obcaecati provident ipsam minus quod, accusamus eos. Laboriosam velit quisquam earum, similique voluptate voluptas. Autem dolore ab nesciunt doloribus rem inventore, suscipit fugit blanditiis veritatis nihil. Nulla laudantium nemo dignissimos laboriosam eligendi. Aliquid, necessitatibus earum soluta sit rerum ea ad perspiciatis nostrum distinctio ex dolorum perferendis iusto nulla tempora alias.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus esse dolore similique quae voluptatibus quod dolores nesciunt doloribus sed cupiditate quidem eius reprehenderit magnam enim nulla ducimus, dolorum rerum, quo veniam dicta maxime. Quibusdam nostrum accusamus exercitationem dolorem quas architecto provident asperiores quam? Delectus, similique consequatur illo molestias fugiat mollitia obcaecati provident ipsam minus quod, accusamus eos. Laboriosam velit quisquam earum, similique voluptate voluptas. Autem dolore ab nesciunt doloribus rem inventore, suscipit fugit blanditiis veritatis nihil. Nulla laudantium nemo dignissimos laboriosam eligendi. Aliquid, necessitatibus earum soluta sit rerum ea ad perspiciatis nostrum distinctio ex dolorum perferendis iusto nulla tempora alias.
+      </p>
+      
+    </div>
   )
 }
